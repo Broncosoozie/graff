5bbd9ffc648a5fc774f1cd932d65cba97e84a1db
Broncosoozie <broncosoozie@gmail.com>
2019-05-01 07:44:32 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var _ = require('lodash');
+
+app.use(express.static(__dirname + '/public'));
+
+var wholeWordListFile = require(__dirname + '/data/wordlist.json');
+var baseWordList = wholeWordListFile.baseWords;
+var leagueChampsList = wholeWordListFile.leagueChamps
+var currentDrawer;
+
+var wordList = baseWordList + leagueChampsList;
+
+// app.get('/', function(request, response) {
+//   response.sendFile(__dirname + 'index.html');
+// });
+
+io.on('connection', function(socket) {
+  console.log('User connected with Socket ID: ' + socket.id);
+
+  socket.on('chat message', function(username, message) {
+    console.log('Socket ID: ' + socket.id + ' is Username: ' + username + ' and sending message: ' + message);
+    io.emit('chat message', username + ': ' + message);
+  });
+
+  socket.on('guess message', function(username, message, now) {
+    console.log('Username: ' + username + ' is guessing: ' + message);
+    io.emit('guess message', username, message, now);
+  });
+
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+  socket.on('clear canvas', function() {
+    io.emit('clear canvas');
+  });
+
+  // socket.on('skip word', function(newIndex) {
+  //   socket.broadcast.emit('next word', newIndex);
+  // });
+
+  socket.on('start game', function() {
+    console.log('Game starting...');
+    var wordsToDraw = _.sampleSize(wordlist, 10);
+    console.log('Sending Wordlist: ' + wordsToDraw);
+    console.log('To: ' + socket.id);
+    currentDrawer = socket.id
+    io.emit('clear guess list');
+    io.emit('start new game');
+    io.to(`${currentDrawer}`).emit('your turn', wordsToDraw);
+    socket.broadcast.emit('current word', wordsToDraw[0]);
+    // socket.broadcast.emit('wordlist', wordsToDraw);
+  });
+
+  socket.on('current word', function(currentWord) {
+    socket.broadcast.emit('current word', currentWord);
+  });
+
+  socket.on('game win', function() {
+    io.emit('game win');
+  });
+
+  socket.on('skip word', function() {
+    io.emit('skip word');
+  });
+
+  socket.on('correct guess', function() {
+    // socket.broadcast.emit('correct guess');
+    io.to(`${currentDrawer}`).emit('correct guess');
+    // io.emit('correct guess');
+  });
+
+  socket.on('highlight guess', function(id) {
+    socket.broadcast.emit('highlight guess', id);
+  });
+
+  socket.on('disconnect', function() {
+    console.log('User with Socket ID: ' + socket.id + ' disconnected');
+  });
+});
+
+http.listen(9600, function() {
+  console.log('listening on *:9600');
+});
---
+++ public/game_logic.js
@@ -0,0 +1,188 @@
+'use strict';
+
+$(function () {
+  var socket = io();
+  var wordList = [];
+  var currentWord;
+  var gameLogic = {youAreDrawing: false};
+
+  drawing.initialize(socket, gameLogic);
+  $('#skip').attr("disabled", true);
+
+  function correctGuess(guess, actual) {
+    var regex = new RegExp(".*" + actual + ".*", "i", "g");
+    return guess.match(regex) !== null;
+  };
+
+  $('#name-set').submit(function(e) {
+    e.preventDefault();
+    socket.emit('name change', $('#name').val());
+    return false;
+  });
+
+  $('#clear').click(function(e) {
+    e.preventDefault();
+    socket.emit('clear canvas');
+    return false;
+  });
+
+  $('#skip').click(function(e) {
+    var currentIndex = _.findIndex(wordList, function(word) {
+      return word == $('.word').text();
+    });
+    var newIndex;
+    if (currentIndex == (wordList.length - 1)) {
+      newIndex = 0;
+    } else {
+      newIndex = currentIndex + 1;
+    }
+    $('.word').text(wordList[newIndex]);
+
+    console.log("Skipping! New word: " + wordList[newIndex]);
+
+    socket.emit('skip word');
+    socket.emit('clear canvas');
+    // socket.emit('skip word', newIndex);
+    socket.emit('current word', wordList[newIndex]);
+    currentWord = wordList[newIndex]; // Doesn't matter for drawer, they shouldnt' guess anyway, but they do need to see what is correct
+    return false;
+  });
+
+  $('#start-game').click(function(e) {
+    socket.emit('start game');
+    return false;
+  });
+
+  $('#main-chat').submit(function(e) {
+    e.preventDefault();
+    var username = $('#name').val();
+    var message = $('#m').val();
+    socket.emit('chat message', username, message);
+    $('#m').val('');
+    return false;
+  });
+
+  $('#guess-form').submit(function(e) {
+    e.preventDefault();
+    if (!gameLogic.youAreDrawing) {
+      var username = $('#name').val();
+      var guess = $('#guess-m').val();
+      socket.emit('guess message', username, guess, $.now());
+    }
+    $('#guess-m').val('');
+    return false;
+  });
+
+  socket.on('your turn', function(wordsToDraw) {
+    console.log('your turn!');
+    console.log(wordsToDraw);
+    wordList = wordsToDraw;
+    currentWord = wordList[0];
+    // socket.emit('current word', currentWord);
+    $('.word').text(wordList[0]);
+    gameLogic.youAreDrawing = true;
+    $('#skip').attr('disabled', false);
+  });
+
+  socket.on('start new game', function() {
+    $("div#game-start").fadeIn(300).delay(1500).fadeOut(400);
+    $('#start-game').attr("disabled", true);
+    if (!gameLogic.youAreDrawing) {
+      $('#skip').attr("disabled", true);
+    }
+  });
+
+  socket.on('clear guess list', function() {
+    $('#guess-messages').empty();
+  });
+
+  socket.on('chat message', function(message) {
+    $('#messages').append($('<li>').text(message));
+  });
+
+  socket.on('guess message', function(username, message, now) {
+    var fullMessage = username + ": " + message;
+    var listItem;
+
+    if (correctGuess(message, currentWord)) {
+    // if (message == currentWord) {
+      listItem = $('<li class="correct">').text(fullMessage);
+      listItem.attr('id', now);
+      $("div#correct-guess").text("Word was: " + currentWord + ", correct!");
+      $("div#correct-guess").fadeIn(300).delay(500).fadeOut(400);
+      if (gameLogic.youAreDrawing) {
+        socket.emit('correct guess', currentWord);
+      }
+    } else {
+      listItem = $('<li>').text(fullMessage);
+      listItem.attr('id', now);
+      listItem.click(function() {
+        if (gameLogic.youAreDrawing) {
+          socket.emit('highlight guess', listItem.attr('id'));
+          $('#' + listItem.attr('id')).toggleClass('highlight');
+        }
+      });
+    }
+
+    $('#guess-messages').append(listItem);
+    $('#guess-box').animate({ scrollTop: $('#guess-messages').prop("scrollHeight") }, 500);
+  });
+
+  socket.on('current word', function(newWord) {
+    currentWord = newWord;
+  });
+
+  socket.on('highlight guess', function(id) {
+    $('#' + id).toggleClass('highlight');
+  });
+
+  socket.on('game win', function() {
+    gameLogic.youAreDrawing = false;
+    currentWord = null;
+    $('.word').text("");
+    $('#start-game').attr("disabled", false);
+    $('#skip').attr("disabled", true);
+    socket.emit('clear canvas');
+
+    $("div#game-win").fadeIn(300).delay(3000).fadeOut(400);
+  });
+
+  // socket.on('wordlist', function(wordsBeingDrawn) {
+  //   wordList = wordsBeingDrawn;
+  //   currentWord = wordList[0];
+  // });
+
+  // socket.on('next word', function(currentIndex) {
+  //   currentWord = wordList[currentIndex];
+  // });
+
+  socket.on('correct guess', function() {
+    var currentIndex = _.findIndex(wordList, function(word) {
+      return word == $('.word').text();
+    });
+    var newIndex;
+    if (currentIndex == (wordList.length - 1)) {
+      newIndex = 0;
+    } else {
+      newIndex = currentIndex;
+    }
+
+    _.pullAt(wordList, currentIndex);
+
+    if (wordList.length == 0) {
+      socket.emit('game win');
+    } else {
+      console.log('Correct! New word: ' + wordList[newIndex]);
+      $('.word').text(wordList[newIndex]);
+      socket.emit('clear canvas');
+      socket.emit('current word', wordList[newIndex]);
+      currentWord = wordList[newIndex];
+    }
+    return false;
+  });
+
+  socket.on('skip word', function() {
+    $("div#skip-word").text("Word skipped!");
+    $("div#skip-word").fadeIn(300).delay(500).fadeOut(400);
+  });
+});
